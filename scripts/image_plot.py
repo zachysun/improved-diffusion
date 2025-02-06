a5f5c4ea0da73bb96d7934a4321138ee20a39c30
ZachySun <1754976743@qq.com>
2025-02-06 20:48:10 +0800

    add parser in image_plot.py

--- scripts/image_plot.py
+++ scripts/image_plot.py
@@ -1,19 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Plot images from npz file')
+parser.add_argument('--data_dir', type=str, required=True, help='Path to npz file')
+args = parser.parse_args()
 
 # Load the npz file
-data_path = r"C:\Myfiles\OPENAI_DIR\samples_36x128x128x1.npz"
-data = np.load(data_path)
+data = np.load(args.data_dir)
 
-# Get the array from the npz file
-images = data.f.arr_0  
+# Get the array from the npz file and process it
+images = data.f.arr_0
 
-rows = 3
-cols = 3
+rows = 2
+cols = 2
+
 fig, axes = plt.subplots(rows, cols, figsize=(10, 10))
 
 # Plot each image
-for i in range(9):
+for i in range(4):
     row = i // cols
     col = i % cols
     
@@ -21,7 +27,7 @@ for i in range(9):
     
     axes[row, col].imshow(img, cmap='gray')
     axes[row, col].axis('off')
-    axes[row, col].set_title(f'Image {i+1}')
+    axes[row, col].set_title(f'Sampled Image {i+1}')
 
 plt.tight_layout()
 plt.show()
